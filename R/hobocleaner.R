29c528acca2c8ed8b5b14a846cb793fb204fbef3
ricardoi <ralcala@ufl.edu>
2022-10-21 11:38:01 -0700

    mean instead of unique hobocleaner

--- R/hobocleaner.R
+++ R/hobocleaner.R
@@ -17,19 +17,25 @@
 #' @export
 
 hobocleaner <- function(file){
-  temp <- file[,2:6] |> 
-    unique() 
+  temp <- file[,2:6]
   init <- dim(file)[1]
+  # formating hobo dates to UTC
   temp$Date.Time <- gsub(":", "-", temp$Date.Time)
   temp$Date.Time <- gsub(" ", "-", temp$Date.Time)
   temp$Date.Time <- gsub("^", "20", temp$Date.Time)
   temp$Date.Time <- lubridate::ymd_hms(temp$Date.Time, truncated = 1)
-  temp$Date.Time <- round(temp$Date.Time, units = "mins") |>
-      unique()
+  temp$Date.Time <- round(temp$Date.Time, units = "mins")
   temp$Date <- as.Date(temp$Date.Time)
   temp = temp |> 
           dplyr::arrange(Date.Time)
-  clean <- dim(temp)[1]
+  # mean repeated measurements by date
+  dat <- temp |>
+    dplyr::group_by(Date.Time) |>
+    dplyr::summarise(Wetness = mean(Wetness, na.rm = T),
+                        Temp = mean(Temp, na.rm = T),
+                          RH = mean(RH, na.rm = T),
+                        Rain = mean(Rain, na.rm = T))
+  clean <- dim(dat)[1]
   cat(paste0(" proccesed: ", init, " all entries", "\n cleaned: ", init-clean, " duplicated entries", 
              "\n   total: ", clean, " unique entries"))
   return(temp)
