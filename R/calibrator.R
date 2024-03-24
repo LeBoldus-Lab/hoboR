1b16fd01c6cf6cb0190420a97db2ff3f534d7bba
ricardoi <ralcala@ufl.edu>
2024-03-23 17:15:46 -0700

    sky calibrator

--- R/calibrator.R
+++ R/calibrator.R
@@ -17,9 +17,43 @@
 #' calibratedcsv <- calibritor(calibration-data)
 #' @export
 
-calibrator <- function(data, formula = "y = a + b", times = c(12:00, 13:00, 14:00)){
-  
-}
+times <- c("2022-03-22 01:00", "2022-03-22 02:00", "2022-03-22 03:00", "2022-03-22 04:00",
+           "2022-03-22 05:00", "2022-03-22 06:00", "2022-03-22 07:00", "2022-03-22 08:00",
+           "2022-03-22 09:00")
+# I need to add the same time slot for all hobos used for calibration
+# Do the substraction from hobo 1 to the rest of hobos within the same time slot 
+# Create a list of input files 
 
-# plot(1:dim(hobocleaned)[1], hobocleaned$Temperature, pch=4)
-
+calibrator <- function(data, formula = "y = a + b", columns= c(2, 7, 12), times = times){
+  # from character to UTC times
+  time=as.POSIXct(times, tz = "UTC")
+  # subset by times of interest
+  x <- lapply(data, function(df) {
+    df[df$Date %in% time, ]
+  })
+  # get the base columns - probably to remove
+  # base <- lapply(x, function(df){
+  #   df[1,columns]
+  # })
+  base <- x[[1]][,columns]
+  # subsampling only columns of interest
+  sall <- lapply(x, function(df){
+    df[, columns]
+  })
+  # subtracting hobo one from other hobo's  
+  corr <- lapply(sall, function(df){
+              base - df
+              })
+  # get the mean difference by hobo
+  res <- lapply(corr, function(df){
+          mean <- sapply(df, mean, na.rm = TRUE) 
+          as.data.frame(mean)
+    })
+  res
+  # to a dataframe
+  calibration <- do.call(cbind, res)
+  # present results
+  results <- round(t(calibration), 7)
+  rownames(results) <- paste0("hobo", 1:length(x))
+  return(results)
+}rox
--- temp/sky-test.R
+++ temp/sky-test.R
@@ -1,16 +1,27 @@
 library(hoboR)
 
 
-path = "~/Desktop/testsky/canopy08/"
 path = "~/Desktop/testsky/calibration/originalfiles/"
 # the path file exist
 file.exists(path)
-path=paste0(path, "canopy1/")
-# loading all hobo files
-# hobofiles <- hobinder(path) # header and skip col is a new feature
-hobofiles <- hobinder2(path, channels = "ON" ) # channels is a new feature
-head(hobofiles)
 
-hobocleaned <- hobocleaner(hobofiles, format = "ymd")
+#
+folder=paste0(rep("canopy", 24), 1:24)
+
+pathtoread=calibrationfiles=hobocleaned=data=list()
+
+for (i in seq_along(folder)){
+pathtoread[[i]] <- paste0(path, folder[i])
+# loading all hobo files
+calibrationfiles[[i]] <- hobinder(as.character(pathtoread[i]), channels = "ON" ) # channels is a new feature
+data[[i]]=hobocleaned[[i]] <- hobocleaner(calibrationfiles[[i]], format = "ymd")
+}
+
+times <- c("2022-03-22 01:00", "2022-03-22 02:00", "2022-03-22 03:00", "2022-03-22 04:00",
+           "2022-03-22 05:00", "2022-03-22 06:00", "2022-03-22 07:00", "2022-03-22 08:00",
+           "2022-03-22 09:00")
+
+calibrator(data, formula = "y = a + b", columns= c(2, 7, 12), times = times)
+
 
 ho.range(hobocleaned, start="2022-03-21 17:45", end="2022-03-21 18:20")
