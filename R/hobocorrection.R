02d52031c32c358bd56930676aa90c6e226679b7
ricardoi <ralcala@ufl.edu>
2024-04-08 16:47:35 -0700

    final function

--- R/hobocorrection.R
+++ R/hobocorrection.R
@@ -5,8 +5,9 @@
 #' using a base correction to the data
 #' @author Ricardo I Alcala Briseno, \email{alcalabr@@oregonstate.edu}
 #' @param data a list of CVS data containing the hobo 
-#' @param times a series of times <- c("2022-03-22 01:00", "2022-03-22 02:00", "2022-03-22 03:00")
-#' @return a data frame with the difrences for data correction, to use with corrector
+#' @param w.var a column to correct the weather variable e.g., Temperature, RH (relative humidity), or FULL, will use the output of calibrator
+#' @param calibrate a value to correct the weather variable, must be numeric or USEFILE, will use the output of calibrator 
+#' @return a data frame with the differences for data correction, to use with corrector
 #' 
 #' @importFrom dplyr group_by
 #' @importFrom dplyr mutate
@@ -19,9 +20,27 @@
 #' filestocorrect <- calibritor(calibrationfiles)
 #' calibratedfiles <- correction(data, calibration, calibrate=)
 #' @export
-#' 
 
-correction<- function(data) #, calibration, column = "FULL", calibrate = USEFILE)
-{
-  print(data)
-}
+correction<- function(data, w.var = "FULL", calibrate =calibrate){
+  if ( w.var == "FULL" ) {
+    if ( all(colnames(calibrate) %in% colnames(data[[1]]))){
+    # For each row in calibration results apply the correction to the data list 
+      vars <- colnames(calibrate)
+      cmd <- capture.output(
+        cat( "lapply(seq_along(data), function(i) {\n",
+              paste0("data[[i]]$",vars, " <- data[[i]]$", vars, " + calibrate[i,", "'",vars,"'", "]\n"),
+             "return(data[[i]])})"
+             )
+           )
+      cmd <- str2expression(cmd)
+      # evaluating command
+      dat <- eval(cmd)
+      return(dat)
+    } else {
+      print("Weather variables do not match")
+    }
+  } else {
+    data[w.var] <- data[w.var] + as.numeric(calibrate)
+    return(data)
+    }
+} 
